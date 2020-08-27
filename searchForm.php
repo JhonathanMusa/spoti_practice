f03e8882e25e2ed50acaed68d863b3a50dbbde8a
jhonathanmusa <jonathansm891@gmail.com>
2020-08-26 22:50:50 -0500

    first commit

---
+++ searchForm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main_container">
+        <nav class="nav_section">
+            <ul>
+                <li><a class="active" href="home.php">Home</a></li>
+                <li><a href="searchForm.php">Search</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+        <p>Welcome</p>
+    </div>
+</body>
+</html>
