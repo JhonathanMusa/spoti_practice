0d235fd149bf0e21cb09c528432b39ec0e03fb4a
jhonathanmusa <jonathansm891@gmail.com>
2020-08-27 21:15:04 -0500

    second commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="header">
+        <h1>My Website</h1>
+        <p>Resize the browser window to see the efect</p>
+    </div>
+    <div class="topnav">
+            <a href="#">Home</a>
+            <a href="#section_insert">Insert</a>
+            <a href="#">Contact</a>
+            <a class="right" href="#">About</a>
+    </div>
+    <div class="row">
+        <div class="leftcolumn">
+            <div class="card">
+                <h2>TITLE HEADING</h2>
+                <h5>Title Description</h5>
+                <div class="fakeimg" style="height: 200px">Image</div>
+                <p>Some text</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quos aliquam beatae suscipit sapiente nesciunt eaque ullam, voluptatem non deserunt, autem officiis quibusdam iusto soluta repudiandae ea quae minima similique.</p>
+            </div>
+            <div class="card">
+                <h2>TITLE HEADING</h2>
+                <H5>Title Description</H5>
+                <div class="fakeimg" style="height: 200px">Image</div>
+                <p>Some text</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet doloribus eum porro labore rem illum! Tempora cupiditate repellat expedita ab veniam repellendus eligendi, natus alias! Maiores quaerat voluptatum animi atque.</p>
+            </div>
+        </div>
+        <div class="rightcolumn">
+            <div class="card">
+                <h2>About Me</h2>
+                <div class="fakeimg" style="height: 100px">Image</div>
+                <p>Some text about me</p>
+            </div>
+            <div class="card">
+                <h3>Popular Post</h3>
+                <div class="fakeimg"><p>Image</p></div>
+                <div class="fakeimg"><p>Image</p></div>
+                <div class="fakeimg"><p>Image</p></div>
+            </div>
+            <div class="card">
+                <h3>Follow Me</h3>
+                <p>Some text</p>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <h2>Footer</h2>
+    </div>
+</body>
+</html>
